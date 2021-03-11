ade78cb3474a45f1a6b2df8b66edb43b9a65cb80
LE THUY QUYNH <1724010123@student.humg.edu.vn>
2021-03-11 10:01:44 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTQLBTL.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Với action cho phép truy cập không cần kiểm tra đăng nhập
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Kiểm tra đăng nhập trước khi truy cập với action
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
